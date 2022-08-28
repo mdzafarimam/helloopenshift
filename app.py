965d36a9665895342bb0b11ea76da68c8bf39dc3
Md Zafar Imam <mdzaimam@in.ibm.com>
2022-08-28 14:50:41 +0530

    commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/hello")
 def hello():
     return "Hello World!"
 
 
 # driver function
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
